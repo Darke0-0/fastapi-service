b29c51e70f134a2fb2aa07077e7a085ebe638f9f
Darke0-0 <91913010+Darke0-0@users.noreply.github.com>
2023-05-14 14:10:16 +0530

    commit5

--- main.py
+++ main.py
@@ -1,33 +1,83 @@
+from urllib.parse import urljoin
+import json
 import uvicorn
+import ast
 import requests
-from fastapi import FastAPI
-from transformers import pipeline
+import argparse
+from dtype import METADATA
 
-app = FastAPI()
+# Replace this with the value of your endpoint 
+# ENDPOINT_URL = "https://test1-intern-kanishq.demo1.truefoundry.com/v2/models/test1/infer"
+
+# inp = {"inputs":[
+#                 {"name":"array_inputs",
+#                  "datatype":"BYTES",
+#                  "shape":[-1],
+#                  "parameters":{"content_type":"str"},
+#                  "data":["Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!"]},
+#                 {"name":"candidate_labels",
+#                  "datatype":"BYTES",
+#                  "shape":[-1],
+#                  "parameters":{"content_type":"str"},
+#                  "data": ["refund", "legal", "faq"]}
+#                   ],
+#         "outputs":[],
+#         "parameters":{}
+# }
+
+def query_conversion(query:dict):
+    query_list= []
+    query_values = list(query.values())
+    for i in query_values:
+
+        if isinstance(i,str):
+            query_list.append(i)
+
+        elif isinstance(i,list):
+            query_list.append(i)
+
+        elif isinstance(i,dict):
+            converted = list(i.values())[0]
+            query_list.append(converted)
+            
+    return query_list
+
+def input_formatting(query:dict,pipeline:str):
+
+    query_list = query_conversion(query=query)
+    format = dict(METADATA[pipeline])
+
+    inp = []
+    for i in range(len(list(format['inputs']))):
+        ith_query = query_list[i]
+        ith_format = dict(format['inputs'][i])
+        ith_format['data'] = ith_query
+        ith_format['parameters'] = dict(ith_format['parameters'])
+        inp.append(ith_format)
+
+    return {"inputs":inp,"outputs":[],"parameter":{}}
 
 @app.post("/predict")
-async def predict(payload: dict):
-    # convert input to V2 inference protocol
-    # use pipeline_name to select the appropriate conversion logic
-    converted_input = ...
+async def app(query:dict,pipeline:str,endpoint_url:str):
+    
+    inp = input_formatting(query=query, pipeline=pipeline)
+    response = requests.post(endpoint_url,json=inp, headers={"Content-Type": "application/json"})
+    result = response.json()
 
-    # send converted input to deployed model
-    model_url = "https://my-model.com"
-    response = requests.post(model_url, json=converted_input)
+    return result
 
-    # return response to client
-    return response.json()
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Start the FastAPI server.")
-    parser.add_argument("--hf_pipeline", type=str, required=True, help="Name of Hugging Face pipeline to use.")
-    parser.add_argument("--model_deployed_url", type=str, required=True, help="URL of deployed model.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a ArcHydro schema')
+    parser.add_argument('--hf_pipeline' , required=True,
+                        help='pipeline running')
+    parser.add_argument('--endpoint_url' , required=True,
+                        help='url of endopint')
+    parser.add_argument('--query' , required=True,
+                        help='input query')
     args = parser.parse_args()
 
-    # create pipeline based on pipeline_name
-    pipeline_name = args.hf_pipeline
-    pipeline = pipeline(pipeline_name)
+    query = ast.literal_eval(args.query)
 
-    # start server
+    # response = app(query=query, pipeline=args.hf_pipeline, endpoint_url=args.endpoint_url)
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
